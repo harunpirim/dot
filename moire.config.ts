ebdd74fa86e753de3337087443f036162552a5de
harunpirim <43437789+harunpirim@users.noreply.github.com>
2026-04-14 19:06:46 -0500

    Update moire.config.ts

--- moire.config.ts
+++ moire.config.ts
@@ -1,11 +1,11 @@
 export const config = {
-  title: "Moire",
-  author: "Aeris",
+  title: ".dot.",
+  author: "harun",
   theme: "classic",
-  heatmap: true,
+  heatmap: false,
   pageSize: 20,
   order_by: "created",
-  description: "Sync your thoughts from Apple Notes by Shortcuts.",
+  description: "jot down",
   keywords: "memo, thought, sync, apple notes, shortcuts, ios",
   url: "https://moire.blog"
 };
